5ee3c8869f04c2d2256961d28208b087d93b10bb
Nethelbert Blasse <12989615+Gilblasse@users.noreply.github.com>
2020-08-28 13:31:08 -0400

    convereted back to es5

---
+++ src/app/EvalRepo.js
@@ -0,0 +1,92 @@
+// import latestVersion from 'latest-version'
+const latestVersion = require('latest-version')
+
+
+// SCRAPE JAVASCRIPT PACAKGES
+const getPackageJson = async (baseUrl, page, packageType) => {
+    const response = await page.goto(`${baseUrl}${packageType.url}`, { waitUntil: 'networkidle2' } );
+    let errorMessage = "Not Found"
+    let arr = []
+
+    if(response._status == 200){
+        const scrapedData = await page.evaluate(() =>{
+            const repoPackageTable = document.querySelector('table').innerText
+            const repoPackage = JSON.parse(repoPackageTable)
+        
+            return repoPackage
+        })
+
+        const depends = Object.entries(scrapedData.dependencies)
+
+        for(let i=0; i < depends.length; i++ ){
+            const latestV = await latestVersion(depends[i][0])
+            arr.push([...depends[i],latestV])
+        }
+    }
+
+
+    return response._status == 200 ? arr : errorMessage
+}
+
+
+
+
+// SCRAPE RUBY PACAKGES
+const getGemFile = async (baseUrl, page, packageType) => {
+    const response = await page.goto(`${baseUrl}${packageType.url}`, { waitUntil: 'networkidle2' } );
+    let errorMessage = "Not Found"
+    let gems;
+
+    console.log({status: response._status})
+
+    if(response._status == 200){
+        gems = await page.evaluate(() =>{
+            const tableList = document.querySelectorAll('tr')
+            const trs = Object.values(tableList).map(tr => tr.innerText.trim())
+            const gemsRows = trs.filter(row => row.match(/(?<=gem)(.*)(?=')/gs)).filter(r => !r.includes('source') && !r.includes('#'))
+
+            const gemsData = gemsRows.map(gem => {
+                newGem = gem.split(/^(.+?),/)
+                return newGem.length == 1 ? newGem : newGem.slice(1)
+            })
+
+            return gemsData
+        })
+    }
+
+    return response._status == 200 ? gems : errorMessage
+}
+
+
+
+// SCRAPE PYTHON PACKAGES
+const getPythonDependencies = async (baseUrl, page, packageType) => {
+    const response = await page.goto(`${baseUrl}${packageType.url}`, { waitUntil: 'networkidle2' } );
+    let errorMessage = "Not Found"
+    let dependencies;
+
+    if(response._status == 200){
+        dependencies = await page.evaluate(() =>{
+            const tableList = document.querySelector('table').innerText
+            const matches = tableList.match(/(?<= = \[\n)(.*?)(?=])/gs)
+            const pyDepends = matches.map(s=> s.split(/,\n/).filter(st=> st.trim().length !== 0)).flat().map(s => s.trim())
+
+            depends = pyDepends.map(s => {
+                idx = s.search(/[^'\d a-z | A-Z ]\W/)
+                return idx == -1 ? [s] : [ s.substring(0,idx),s.substring(idx) ]
+            })
+
+            return depends
+        })
+    }
+    
+    return response._status == 200 ? dependencies : errorMessage
+}
+
+
+
+export const languageType = {
+    "JavaScript": {url: "/blob/master/package.json", exe: getPackageJson},
+    "Ruby": {url: "/blob/master/Gemfile", exe: getGemFile},
+    "Python": {url: "/blob/master/setup.py", exe: getPythonDependencies}
+}
---
+++ src/routes/api.js
@@ -0,0 +1,31 @@
+// import express from 'express'
+// import puppeteer from 'puppeteer'
+// import { languageType } from '../app/EvalRepo.mjs'
+
+const express = require('express');
+const puppeteer = require('puppeteer');
+const languageType = require('./app/EvalRepo.mjs');
+
+const router = express.Router();
+
+// Front-End Gets Repo Language through rest: https://api.github.com/repos/:owner/:repo  and sends info via Body parser
+router.post('/repo-packages-info', async (req, res) => {
+
+    const owner = req.body.owner
+    const repo = req.body.repo
+    const language = req.body.language
+
+    const baseUrl = `https://github.com/${owner}/${repo}`
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const packageType = languageType[language]
+
+    let data = packageType ? await packageType.exe(baseUrl, page, packageType) : "Not Found"
+
+    browser.close();
+    res.send({data})
+});
+
+
+module.exports = router
+// export default router;
