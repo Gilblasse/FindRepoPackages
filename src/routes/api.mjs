9cbf911ee27faad742ebeae1d1d511d01a69fed5
Nethelbert Blasse <12989615+Gilblasse@users.noreply.github.com>
2020-08-28 13:23:59 -0400

    changed extensions for heroku

---
+++ src/routes/api.mjs
@@ -0,0 +1,26 @@
+import express from 'express'
+import puppeteer from 'puppeteer'
+import { languageType } from '../app/EvalRepo.mjs'
+
+const router = express.Router();
+
+// Front-End Gets Repo Language through rest: https://api.github.com/repos/:owner/:repo  and sends info via Body parser
+router.post('/repo-packages-info', async (req, res) => {
+
+    const owner = req.body.owner
+    const repo = req.body.repo
+    const language = req.body.language
+
+    const baseUrl = `https://github.com/${owner}/${repo}`
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const packageType = languageType[language]
+
+    let data = packageType ? await packageType.exe(baseUrl, page, packageType) : "Not Found"
+
+    browser.close();
+    res.send({data})
+});
+
+
+export default router;
