f5c757d7ce58a37acb06e7daa8cee521782479de
Nethelbert Blasse <12989615+Gilblasse@users.noreply.github.com>
2020-08-28 07:09:17 -0400

    added additional scraping methods for (python: setup.py file,  ruby: Gemfile)

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ const app = express();
 const port = process.env.port || 4000
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers','*')
+
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({});
+    }
+    next();
+})
 // initialize routes
 app.use('/api/v1', require('./routes/api'));
 
@@ -15,3 +27,5 @@ app.use('/api/v1', require('./routes/api'));
 app.listen(port, function(){
     console.log(`Now listening for requests on - http://localhost:${port}/`);
 });
+
+module.exports = app;
